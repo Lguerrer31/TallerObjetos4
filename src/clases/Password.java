c141457ac9eed00247b61d4c35385a594637508b
Laura Guerrero <lauraguerrero3199@hotmail.com>
2016-09-14 23:43:03 -0500

    Adelantos

---
+++ src/clases/Password.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clases;
+
+/**
+ *
+ * @author coste
+ */
+public class Password {
+    
+    private long contraseña;
+
+    public Password(long contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    public long getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(long contraseña) {
+        this.contraseña = contraseña;
+    }
+    
+    
+    
+}
